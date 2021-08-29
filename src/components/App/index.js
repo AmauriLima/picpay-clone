3f883a15fb496dec49f9eae4ea9549c38b0edff7
AmauriLima <amauri.plimaj@gmail.com>
2021-08-29 13:20:06 -0300

    init: starting project

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Text } from './styles';
+
+export default function App() {
+  return (
+    <Container>
+      <Text>Hello World!</Text>
+    </Container>
+  );
+}
---
+++ src/components/App/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`;
+
+export const Text = styled.Text`
+  color: #000;
+`;
